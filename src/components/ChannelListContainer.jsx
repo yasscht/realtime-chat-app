c368afef68a4d96b0c673522665d8dd039afcd91
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-14 15:23:07 +0000

    implementation of the logout function

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,10 +3,21 @@ import Cookies from "universal-cookie";
 import HospitalIcon from "../assets/hospital.png";
 import LogoutIcon from "../assets/logout.png";
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+const cookies = new Cookies();
 const ChannelListContainer = () => {
+  const logout = () => {
+    cookies.remove("token");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("userId");
+    cookies.remove("phoneNumber");
+    cookies.remove("avatarUrl");
+    cookies.remove("hashedPassword");
+    window.location.reload();
+  };
   return (
     <>
-      <SideBar />
+      <SideBar logout={logout} />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
@@ -47,14 +58,14 @@ const CompanyHeader = () => (
     <p className="channel-list__header__text">Medical Pager</p>
   </div>
 );
-const SideBar = () => (
+const SideBar = ({ logout }) => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
       <div className="icon1__inner">
         <img src={HospitalIcon} alt="hospital" width="30" />
       </div>
     </div>
-    <div className="channel-list__sidebar__icon2">
+    <div className="channel-list__sidebar__icon2" onClick={logout}>
       <div className="icon1__inner">
         <img src={LogoutIcon} alt="Logout" width="30" />
       </div>
